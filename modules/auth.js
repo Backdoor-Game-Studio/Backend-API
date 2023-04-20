cf757cf7b3e1a16170098be59ad73f715d30399e
Attila Takács <attila.takacs@interien.com>
2023-04-20 17:31:06 +0200

    make phone server comatible

--- modules/auth.js
+++ modules/auth.js
@@ -1,6 +1,6 @@
 const env = require("dotenv").config();
 const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 
 const validator = require("./validators");
 
